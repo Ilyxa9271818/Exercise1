9419fa7318c2e510b3df854a8462c1659ec39f7b
Konstantin <85631322+Ilyxa9271818@users.noreply.github.com>
2022-02-25 21:10:13 +0300

    Add files via upload

--- src/test/java/org/simbirsoft/pages/LoginPage.java
+++ src/test/java/org/simbirsoft/pages/LoginPage.java
@@ -15,7 +15,7 @@ public class LoginPage {
     @FindBy(xpath = "//*[contains(@id, 'passp-field-login')]")
     WebElement loginField;
 
-    @FindBy(xpath = "//*[contains(text(), 'Войти')]/..")
+    @FindBy(xpath = "//*[@id=\"index-page-container\"]/div/div[2]/div/div/div[4]/a[2]")
     WebElement loginButton;
 
     @FindBy(xpath = "//*[contains(@id, 'passp-field-passwd')]")
